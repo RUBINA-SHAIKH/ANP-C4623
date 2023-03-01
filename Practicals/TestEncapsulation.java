595fd034551f18af16a40642df360a942caf3098
Rubina Shaikh <123443245+RUBINA-SHAIKH@users.noreply.github.com>
2023-03-01 16:56:59 +0530

    Add files via upload

---
+++ Practicals/TestEncapsulation.java
@@ -0,0 +1,16 @@
+package corejava;
+
+public class TestEncapsulation {
+
+	public static void main(String[] args) 
+	{
+       // creating object of class Encapsulationex
+		Encapsulationex a = new Encapsulationex();
+		// object is used to access the methods of a class
+		a.setId(1);
+		a.setName("Ruby");
+	    System.out.println("Id:"+a.getId()+"\n"+"Name:"+a.getName());
+		
+	}
+
+}
