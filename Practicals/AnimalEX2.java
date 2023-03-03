61c7e0b12b9a0aa9a4e8697e531b895fbe605798
Rubina Shaikh <123443245+RUBINA-SHAIKH@users.noreply.github.com>
2023-03-03 20:44:05 +0530

    Add files via upload

---
+++ Practicals/AnimalEX2.java
@@ -0,0 +1,20 @@
+package corejava;
+
+abstract class AnimalEX2
+{
+	protected int Age;
+	
+public AnimalEX2(int age) {
+		super();
+		Age = age;
+	}
+
+public int getAge() {
+	return Age;
+}
+
+public void setAge(int age) {
+	Age = age;
+}
+       
+}
