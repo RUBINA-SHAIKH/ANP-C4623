f66bbd7140e5351503a2d606214d7a32317a339c
Rubina Shaikh <123443245+RUBINA-SHAIKH@users.noreply.github.com>
2023-03-13 21:15:26 +0530

    Update Myinterface.java

--- Practicals/Myinterface.java
+++ Practicals/Myinterface.java
@@ -2,21 +2,21 @@ package corejava;
 
 interface Myinterface
 {
-	public void doSomething();
+	public void doSomething();//interface method() without body
 }
 
-class MyClass implements Myinterface
+class MyClass implements Myinterface //creating class MyClass
 {
-	public void doSomething()
+	public void doSomething()//body of interface method sound
 	{
 		System.out.println("Doing Something...");
 	}
 }
-class MyMain
+class MyMain // creating class with mymain method()
 {
 	public static void main(String[] args)
 	{
-		MyClass mc = new MyClass();
-		mc.doSomething();
+		MyClass mc = new MyClass();//creating obj of class myclass 
+		mc.doSomething();//calling method() of interface
 	}
 }
